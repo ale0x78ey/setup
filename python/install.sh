a23b8438be3e18f68daf4ade789c856174a51345
Alexey Ulyanov <ale0x78ey@yandex.ru>
2021-01-10 18:53:55 +0300

    add pip dependencies

--- python/install.sh
+++ python/install.sh
@@ -7,15 +7,23 @@ install_setup
 install curl \
         python3
 
+if [[ "`uname -s`" == "Linux" ]]; then
+  install build-essential \
+          libffi-dev      \
+          libssl-dev      \
+          python3-dev     \
+          python3-venv
+fi
+
 if ! `python3 -m pip &>/dev/null`; then
   curl https://bootstrap.pypa.io/get-pip.py | python3 -
 fi
 
-if [[ "`uname -s`" == "Linux" ]]; then
-  install python3-venv
-fi
-
 python3 -m pip install --user -U pip
+
+python3 -m pip install --user \
+  setuptools \
+  wheel
 
 #
 # Remote debugging based on pdb.
@@ -26,3 +34,10 @@ python3 -m pip install --user rpdb
 
 install rlwrap telnet
 # Ex: $ rlwrap telnet localhost 4444
+
+#
+# MySQL support
+#
+if [[ "`uname -s`" == "Linux" ]]; then
+  install libmysqlclient-dev
+fi
