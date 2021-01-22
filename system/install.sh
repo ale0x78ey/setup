82535442f335acda14182e3cd148eaa1afa9fa66
Alexey Ulyanov <ale0x78ey@yandex.ru>
2021-01-22 18:02:05 +0300

    install ssh config

--- system/install.sh
+++ system/install.sh
@@ -4,6 +4,10 @@ set -x
 source common/install.sh
 install_setup
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+
 if [[ "`uname -s`" == "Linux" ]]; then
   install openssh-server
 fi
+
+install_ln ${SCRIPT_DIR}/ssh/config ~/.ssh/config
