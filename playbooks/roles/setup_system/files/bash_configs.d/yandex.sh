e0b28c3cc5dce36fc87f3ac8a7ca36fa34a7e6d5
Alexey Ulyanov <ale0x78ey@yandex.ru>
2021-05-29 18:57:51 +0000

    support yandex cloud cli

---
+++ playbooks/roles/setup_system/files/bash_configs.d/yandex.sh
@@ -0,0 +1,9 @@
+# Yandex Cloud CLI.
+
+if [ -f "${HOME}/yandex-cloud/path.bash.inc" ]; then
+  . "${HOME}/yandex-cloud/path.bash.inc"
+fi
+
+if [ -f "${HOME}/yandex-cloud/completion.bash.inc" ]; then
+  . "${HOME}/yandex-cloud/completion.bash.inc"
+fi
