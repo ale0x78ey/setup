de7501d109c0ca6d4fa90d8c2f47c865fe5c522b
Alexey Ulyanov <ale0x78ey@yandex.ru>
2022-03-08 14:28:15 +0300

    update

---
+++ roles/setup_system/files/bash_configs.d/kubernetes.sh
@@ -0,0 +1,7 @@
+# Krew is the package manager for kubectl plugins.
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# if type minikube &> /dev/null; then
+#   minikube start
+#   eval "$(minikube docker-env)"
+# fi
--- roles/setup_system/files/bash_configs.d/python.sh
+++ roles/setup_system/files/bash_configs.d/python.sh
@@ -1,6 +1,13 @@
+export PATH="$HOME/.poetry/bin:$PATH"
+
 # Don't use .pyc и .pyo.
 export PYTHONDONTWRITEBYTECODE=1
 
 if type pyenv &> /dev/null; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
+
+  export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+  eval "$(pyenv virtualenv-init -)"
 fi
+
