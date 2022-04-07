caecf2e4e78a8d577ce260942e0686b03e686a3b
Alexey Ulyanov <ale0x78ey@yandex.ru>
2022-04-07 07:09:57 +0000

    add yandex completion

--- roles/setup_system/files/bash_configs.d/yandex.sh
+++ roles/setup_system/files/bash_configs.d/yandex.sh
@@ -1,1 +1,11 @@
 alias ycs3='aws s3 --endpoint-url=https://storage.yandexcloud.net'
+
+# The next line updates PATH for Yandex Cloud CLI.
+if [ -f $HOME/yandex-cloud/path.bash.inc ]; then
+  . $HOME/yandex-cloud/path.bash.inc
+fi
+
+# The next line enables shell command completion for yc.
+if [ -f $HOME/yandex-cloud/completion.bash.inc ]; then
+  . $HOME/yandex-cloud/completion.bash.inc
+fi
